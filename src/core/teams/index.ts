79c930285e878b81fdaa4a5e8a17ca4fd62fdc9d
Igor Costa <igor@autohand.ai>
2026-02-25 11:08:52 +1300

    feat(teams): add barrel export for teams module

---
+++ src/core/teams/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright 2025 Autohand AI LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { TeamManager } from './TeamManager.js';
+export { TaskManager } from './TaskManager.js';
+export { TeammateProcess } from './TeammateProcess.js';
+export { MessageRouter } from './MessageRouter.js';
+export { ProjectProfiler } from './ProjectProfiler.js';
+export * from './types.js';
